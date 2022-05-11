b52f8f9e3a5d48ffd32b4c85672f9aac512fc342
enosis-protap <protap.ghosh@enosisbd.com>
2022-05-11 23:26:48 +0600

    Added LIS code.

---
+++ DP/LIS_StrictlyIncreasing.cpp
@@ -0,0 +1,34 @@
+int lisStrictlyIncreasing(vector<int>& nums) {
+    int n = nums.size();
+    assert(n > 0);
+    int mxNum = nums[0], mnNum = nums[0];
+    for(int i=1;i<n;i++){
+        mxNum = max(mxNum, nums[i]);
+        mnNum = min(mnNum, nums[i]);
+    }
+
+    vector<int>inc(n+1, mxNum + 1);
+    inc[0] = mnNum - 1;
+    
+    int ans = 0;
+    for(int i=0;i<n;i++){
+        int low = 0, high = n;
+        
+        while(high - low > 1){
+            int mid = (low + high) >> 1;
+            
+            if(inc[mid] < nums[i]){
+                low = mid;
+            }
+            else{
+                high = mid;
+            }
+        }
+        
+        int idx = (inc[low] >= nums[i] ? low : high);
+        inc[idx] = nums[i];
+        ans = max(ans, idx);
+    }
+    
+    return ans;
+}
